70a3956944ea1aaed808283beb87626624ce6062
Justin Huertas <justin.hv08@gmail.com>
2024-09-19 11:10:25 -0500

    resolver problemas de produccion

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -9,36 +9,41 @@ import HelpPage from '@/pages/HelpPage';
 const CountryPage = lazy(() => import('@/pages/CountryPage'));
 const CountriesPage = lazy(() => import('@/pages/CountriesPage'));
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <CountriesLayout />,
+			children: [
+				{
+					path: '/',
+					index: true,
+					element: (
+						<Suspense fallback={<Loader />}>
+							<CountriesPage />
+						</Suspense>
+					),
+				},
+				{
+					path: '/help',
+					element: <HelpPage />,
+				},
+				{
+					path: '/country/:code',
+					element: (
+						<Suspense fallback={<Loader />}>
+							<CountryPage />
+						</Suspense>
+					),
+				},
+				{
+					path: '/about-us',
+					element: <AboutUsPage />,
+				},
+			],
+		},
+	],
 	{
-		path: '/',
-		element: <CountriesLayout />,
-		children: [
-			{
-				path: '/',
-				index: true,
-				element: (
-					<Suspense fallback={<Loader />}>
-						<CountriesPage />
-					</Suspense>
-				),
-			},
-			{
-				path: '/help',
-				element: <HelpPage />,
-			},
-			{
-				path: '/country/:code',
-				element: (
-					<Suspense fallback={<Loader />}>
-						<CountryPage />
-					</Suspense>
-				),
-			},
-			{
-				path: '/about-us',
-				element: <AboutUsPage />,
-			},
-		],
+		basename: '/',
 	},
-]);
+);
